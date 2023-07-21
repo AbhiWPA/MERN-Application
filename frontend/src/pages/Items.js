7c3b49a4ce4c679205866bc310daaa6cccad93b8
AbhiWPA <abhiashinsa770@gmail.com>
2023-07-21 02:58:43 -0700

    Content updated

--- frontend/src/pages/Items.js
+++ frontend/src/pages/Items.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/Layout/Header'
 import { TextField } from '@mui/material'
 import Form from '../components/Form/Form'
@@ -6,18 +6,47 @@ import AddCircleIcon from "@mui/icons-material/AddCircle";
 import AutorenewIcon from "@mui/icons-material/Autorenew";
 import DeleteIcon from "@mui/icons-material/Delete";
 import BackspaceIcon from "@mui/icons-material/Backspace";
+import Table from '../components/Table/Table';
 
 const Items = () => {
+  const [allCustomersList, setAllCustomersList] = useState([]);
+
+  const handleTableRowClick = (tableRow) => {
+    console.log(tableRow);
+  }
+
+  // useEffect(() => {
+  //   let customerArray = [
+  //     [
+  //       "C001", "Kamal", "Galle", "077777777"
+  //     ],
+
+  //     [
+  //       "C001", "Kamal", "Galle", "077777777"
+  //     ],
+  //   ];
+
+  //   setAllCustomersList(customerArray)
+  // });
+
+  const tblHeaders = ["Header 1", "Header 2", "Header 3"];
+  const tblData = [
+    ["Data 1", "Data 2", "Data 3"],
+    ["Data 4", "Data 5", "Data 6"],
+    // Add more data rows as needed
+  ];
+  const tableHeight = "300px"; // Adjust the height as needed
+
   return (
     <div>
       <Header />
 
       <section>
-        <div style={{display: 'inline-block', marginTop: '20px'}}>
+        <div style={{display: 'inline-block', marginTop: '10px'}}>
           <h2 className='ml-6 text-3xl text-red-700 font-bold font-sans inline-block shadow-black shadow-lg'>MANAGE ITEMS</h2>
         </div>
 
-        <div className='absolute top-0 bottom-0 left-0 right-0 m-auto ms-auto shadow-lg shadow-black h-56 w-4/5 rounded-lg'>
+        <div className='absolute top-40 left-0 right-0 m-auto ms-auto shadow-lg shadow-black h-56 w-4/5 rounded-lg'>
           <h3 className='text-xl text-blue-800 font-serif font-bold text-center mt-3'>Save | Update | Delete Item</h3>
 
           <div className='absolute top-0 bottom-0 left-0 right-0 m-auto ms-auto w-11/12 h-2/4 inline-block space-x-6'>
@@ -106,6 +135,22 @@ const Items = () => {
         </div>
 
       </section>
+
+      <div className='absolute top-2/3 left-0 right-0 m-auto w-11/12 h-1/3'>
+      <Table
+          tblName="All Item Details"
+          tblHeight="auto"
+          tblHeaders={[
+            "Item Code",
+            "Description",
+            "Unit Price",
+            "Quantity",
+          ]}
+          tblData={allCustomersList.map((customerArray) => customerArray)}
+          handleTblRowClick={handleTableRowClick}
+        />
+      </div>
+
       
     </div>
   )
